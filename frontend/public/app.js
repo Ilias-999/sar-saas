1edf926949381955874a9eae3c10491821655f34
Ilias-999 <iliasouandouri@gmail.com>
2026-05-01 21:54:27 +0200

    fix: cicd and file upload

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = `http://${window.location.hostname}:8000`;
 
 const sarFileInput = document.getElementById("sarFileInput");
 const selectFileButton = document.getElementById("selectFileButton");
